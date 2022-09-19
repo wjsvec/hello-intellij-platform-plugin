d98dca273130be9526fb1d3b6ce64d552e5fa4dc
GitHub Action <action@github.com>
2022-09-19 06:56:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/wjsvec/hellointellijplatformplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wjsvec/hellointellijplatformplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wjsvec.hellointellijplatformplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wjsvec.hellointellijplatformplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wjsvec/hellointellijplatformplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wjsvec/hellointellijplatformplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wjsvec.hellointellijplatformplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wjsvec.hellointellijplatformplugin.MyBundle
 
 class MyApplicationService {
 
